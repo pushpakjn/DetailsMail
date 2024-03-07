2c3ba1b7815dbe3dccfb8f084f6feca4bfc4b344
Pushpak Jain <84683252+pushpakjn@users.noreply.github.com>
2024-03-08 02:46:55 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -124,7 +124,7 @@ app.post("/send-email", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("Server is running on port 5000");
 });
